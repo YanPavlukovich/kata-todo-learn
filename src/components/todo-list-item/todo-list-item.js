4a7be5786f204bf83855c6d1a1c8c0da86c01dc5
YanPavlukovich <filin@gmail.com>
2023-02-27 11:59:09 +0300

    переписал два компонента на классы

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false }) => {
-  const myStyle = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal',
-  };
-  return (
-    <span className="todo-list-item">
-      <span className="todo-list-item-label" style={myStyle}>
-        {label}
+export default class TodoListItem extends Component {
+  render() {
+    const { label, important = false } = this.props;
+    const myStyle = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal',
+    };
+    return (
+      <span className="todo-list-item">
+        <span className="todo-list-item-label" style={myStyle}>
+          {label}
+        </span>
+        <button id="margin" type="button" className="btn btn-outline-success btn-sm">
+          <i className="bi bi-exclamation-square" />
+        </button>
+
+        <button type="button" className="btn btn-outline-danger btn-sm ">
+          <i className="bi bi-trash" />
+        </button>
       </span>
-      <button id="margin" type="button" className="btn btn-outline-success btn-sm">
-        <i className="bi bi-exclamation-square" />
-      </button>
-
-      <button type="button" className="btn btn-outline-danger btn-sm ">
-        <i className="bi bi-trash" />
-      </button>
-    </span>
-  );
-};
-
-export default TodoListItem;
+    );
+  }
+}
