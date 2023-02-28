5f248ad7637596b43c60f6c3a5b196f8230e22a1
YanPavlukovich <filin@gmail.com>
2023-02-28 13:07:22 +0300

    переписал app.js на классы, сделал элемент state-ом и прописал функционал удаления отдельного элемента по нажатию кнопки без изменения setState

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import AppHeader from '../app-header/app-header';
-import SearchPanel from '../search-panel/search-panel';
-import TodoList from '../todo-list/todo-list';
-import ItemStatusFilter from '../item-status-filter/item-status-filter';
+import AppHeader from '../app-header/app-header.js';
+import SearchPanel from '../search-panel/search-panel.js';
+import TodoList from '../todo-list/todo-list.js';
+import ItemStatusFilter from '../item-status-filter/item-status-filter.js';
 
 import './app.css';
 
-const App = () => {
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Build React App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 },
-  ];
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="todo-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+export default class App extends Component {
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Build React App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 },
+    ],
+  };
+
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((item) => item.id === id);
+
+      const newTodoData = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+
+      return {
+        todoData: newTodoData,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="todo-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
       </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
