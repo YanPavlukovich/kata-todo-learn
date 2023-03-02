b4c06e756f2de8ab5e35139c1e19d2a5a077b202
YanPavlukovich <filin@gmail.com>
2023-03-02 23:41:08 +0300

    создал функционал, который отменяет дефолтное поведение onSubmit, теперь созданная форма не пропадает при создании

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,17 +3,26 @@ import React, { Component } from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+  state = {
+    label: '',
+  };
+
+  onLabelChange = (event) => {
+    this.setState({ label: event.target.value });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.props.onItemAdded(this.state.label);
+  };
   render() {
     return (
-      <div className="item-add-form">
-        <button
-          type="submit"
-          className="btn btn-outline-secondary"
-          onClick={() => this.props.onItemAdded('Добавить новый Task')}
-        >
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input type="text" className="form-control" onChange={this.onLabelChange} placeholder="What needs to be done?" />
+        <button type="submit" className="btn btn-outline-secondary">
           Add Item
         </button>
-      </div>
+      </form>
     );
   }
 }
