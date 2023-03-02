06529a68b2304554416717adb99cd2e30b714de4
YanPavlukovich <filin@gmail.com>
2023-03-02 23:51:04 +0300

    сделал input формы контролируемым, теперь после довавления задачи введенный текст очищается автоматически

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -14,11 +14,12 @@ export default class ItemAddForm extends Component {
   onSubmit = (event) => {
     event.preventDefault();
     this.props.onItemAdded(this.state.label);
+    this.setState({ label: '' });
   };
   render() {
     return (
       <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
-        <input type="text" className="form-control" onChange={this.onLabelChange} placeholder="What needs to be done?" />
+        <input type="text" className="form-control" onChange={this.onLabelChange} placeholder="What needs to be done?" value={this.state.label} />
         <button type="submit" className="btn btn-outline-secondary">
           Add Item
         </button>
